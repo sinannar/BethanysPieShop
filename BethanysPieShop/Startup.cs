b36b607935a0cf39d6c48bf91e5f03dcc529bfae
sinan nar <sinan.nar@gmail.com>
2018-09-06 14:35:13 +1200

    piecontroller added, category and pie entities has been implemented, hardcoded repos introduced, dependency injection has been used for repos

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,11 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Setting up dependency Injection
+            services.AddTransient<ICategoryRepository,MockCategoryRepository>();
+            services.AddTransient<IPieRepository,MockPieRepository>();
+
+            // Adding MVC
             services.AddMvc();
         }
 
