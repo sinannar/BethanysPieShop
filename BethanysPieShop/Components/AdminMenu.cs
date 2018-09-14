c5e97b2487d95ef42e87d3a15812046ef939641a
sinan nar <sinan.nar@gmail.com>
2018-09-14 16:40:42 +1200

    adminmenu has been added

---
+++ BethanysPieShop/Components/AdminMenu.cs
@@ -0,0 +1,35 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Components
+{
+    public class AdminMenu:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var menuItems = new List<AdminMenuItem> { new AdminMenuItem()
+                {
+                    DisplayValue = "User management",
+                    ActionValue = "UserManagement"
+
+                },
+                new AdminMenuItem()
+                {
+                    DisplayValue = "Role management",
+                    ActionValue = "RoleManagement"
+                }};
+
+            return View(menuItems);
+        }
+    }
+
+    public class AdminMenuItem
+    {
+        public string DisplayValue { get; set; }
+        public string ActionValue { get; set; }
+    }
+}
