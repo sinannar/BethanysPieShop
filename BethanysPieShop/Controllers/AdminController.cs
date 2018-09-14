84fd0b3a1bbaeba1e07efd651b860cb1de3eed56
sinan nar <sinan.nar@gmail.com>
2018-09-14 15:35:48 +1200

    usermanagement and adduser has been implemented

---
+++ BethanysPieShop/Controllers/AdminController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdminController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UserManagement()
+        {
+            var users = _userManager.Users;
+            return View(users);
+        }
+
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(AddUserViewModel addUserViewModel)
+        {
+            if (!ModelState.IsValid) return View(addUserViewModel);
+
+            var user = new IdentityUser()
+            {
+                UserName = addUserViewModel.UserName,
+                Email = addUserViewModel.Email
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, addUserViewModel.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("UserManagement", _userManager.Users);
+            }
+
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+            return View(addUserViewModel);
+        }
+    }
+}
